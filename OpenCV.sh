3f59985bfaad4d1696464ee31584d6e8ab23fa0b
Git_tips <nexstar1436@gmail.com>
2016-04-24 16:12:58 +0800

    Update OpenCV.sh

--- OpenCV.sh
+++ OpenCV.sh
@@ -3,10 +3,14 @@ sudo apt-get update
 cd
 mkdir _OpenCV
 cd _OpenCV
-sudo apt-get -y install open-server vim git
+sudo apt-get -y install open-server vim git cmake
 
 echo "*************Install the Dependencies*************"
-sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff4-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
+sudo apt-get -y install libopencv-dev build-essential libdc1394-22-dev libjpeg-dev 
+sudo apt-get -y libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22
+sudo apt-get -y libpng12-dev libtiff4-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine-dev
+sudo apt-get -y libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev
+sudo apt-get -y libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
 
 echo "*************Download OpenCV 3.0.0 alpha**********"
 
